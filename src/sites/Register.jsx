5fe93cfaca9bb3983e5cf1e8a3e17005d9b49d8e
petarmarkota <petar.markota@gmail.com>
2024-03-11 23:29:28 +0100

    qm un

--- src/sites/Register.jsx
+++ src/sites/Register.jsx
@@ -64,7 +64,7 @@ export const Register = () => {
               className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="username"
             >
-              Username
+              Username?
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"

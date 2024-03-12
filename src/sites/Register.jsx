b42855406e5893cbc35295f9b7ebc1c2fc1511b9
petarmarkota <petar.markota@gmail.com>
2024-03-12 22:18:47 +0100

    removed sh

--- src/sites/Register.jsx
+++ src/sites/Register.jsx
@@ -67,7 +67,7 @@ export const Register = () => {
               Username
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
               type="text"
               placeholder="Enter your username"

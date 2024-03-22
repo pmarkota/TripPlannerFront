9ef3846920dced7544bd1ff76c74ad2236b51ee6
petarmarkota <petar.markota@gmail.com>
2024-03-22 21:33:32 +0100

    mail verification fixed

--- src/sites/Register.jsx
+++ src/sites/Register.jsx
@@ -46,7 +46,7 @@ export const Register = () => {
               className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="username"
             >
-              Email
+              Emai
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
